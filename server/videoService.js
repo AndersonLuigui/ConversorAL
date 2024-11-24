b89a49e6cbf49d2080d83ece07ea6b2b0ee0bfde
Anderson Luigui <alns1536@hotmail.com>
2024-11-23 21:21:04 -0300

    more update

--- server/videoService.js
+++ server/videoService.js
@@ -1,12 +1,12 @@
 const { exec } = require("child_process");
 const path = require("path");
-''
+
 async function downloadVideo(url) {
     const uniqueName = `video_${Date.now()}.mp4`; // NOME ÚNICO BASEADO NA DATA
     const outputPath = path.resolve(__dirname, "../assets", uniqueName); // CAMINHO ONDE O VIDEO SERÁ SALVO
 
     return new Promise((resolve, reject) => {
-        const command = `yt-dlp -o "${outputPath}" ${url}`; // COMANDO QUE VAI BAIXAR O VÍDEO PELA FUNÇÃO yt-dlp
+        const command = `yt-dlp --no-check-certificate -o "${outputPath}" ${url}`; // COMANDO COM OPÇÃO PARA IGNORAR CERTIFICADO
         exec(command, (error) => {
             if (error) {
                 reject(error);
