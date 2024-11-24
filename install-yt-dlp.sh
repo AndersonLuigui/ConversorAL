1dbca552e03de1bab51b2a41d85fd885fc2879b2
Anderson Luigui <alns1536@hotmail.com>
2024-11-23 21:34:00 -0300

    more update

--- install-yt-dlp.sh
+++ install-yt-dlp.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Atualiza a lista de pacotes
 apt-get update
-apt-get install -y python3-pip  # Instalar o pip, se não estiver instalado
-pip3 install -U yt-dlp  # Instala o yt-dlp com a versão mais recente via pip
+
+# Instala o pip, caso não tenha
+apt-get install -y python3-pip
+
+# Instala o yt-dlp usando pip
+pip3 install -U yt-dlp
+
+# Adiciona o diretório /usr/local/bin ao PATH
+echo 'export PATH=$PATH:/usr/local/bin' >> /etc/profile.d/yt-dlp.sh
+
+# Aplica as alterações no PATH para a sessão atual
+source /etc/profile.d/yt-dlp.sh
