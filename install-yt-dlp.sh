6441fec1574bea308797b481356b4a40cdf76464
Anderson Luigui <alns1536@hotmail.com>
2024-11-23 21:29:00 -0300

    update

--- install-yt-dlp.sh
+++ install-yt-dlp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install -y yt-dlp
-yt-dlp -U  # Força a atualização do yt-dlp para a versão mais recente
+apt-get install -y python3-pip  # Instalar o pip, se não estiver instalado
+pip3 install -U yt-dlp  # Instala o yt-dlp com a versão mais recente via pip
